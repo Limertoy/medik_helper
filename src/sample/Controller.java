e3231ea634d51a1c30af2db37f42a39512f05d40
pkulpinski <malpaonev@gmail.com>
2021-03-23 21:10:52 +0100

    Aktualizacja buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,27 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import javafx.stage.Stage;
 
 public class Controller {
+    @FXML
+    private Button buttonLogin;
+
+    @FXML
+    private Button exit_button;
+
+    @FXML
+    private Button minimalize_button;
+
+    Stage primaryStage;
+
+    @FXML
+    void exit(ActionEvent event) {
+        primaryStage.close();
+    }
+
 
 }
