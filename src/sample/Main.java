94c060ae14cf43f85e20a9abe1d068974bb2ea44
pkulpinski <malpaonev@gmail.com>
2021-03-23 19:44:17 +0100

    Dodana transparentność, oraz zminiłem borderpane na pane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,48 @@
 package sample;
 
+import java.awt.Color;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Medik Helper");
+
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        //grab your root here
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        //move around here
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
         primaryStage.setScene(new Scene(root, 1180, 820));
+        primaryStage.setTitle("MedikHelper");
         primaryStage.show();
     }
 
