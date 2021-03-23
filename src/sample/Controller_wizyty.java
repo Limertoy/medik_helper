300a13f738dc7b75a03c9db615e772d038306cad
pkulpinski <malpaonev@gmail.com>
2021-03-23 23:15:34 +0100

    Poprawa "wizyty"

--- src/sample/Controller_wizyty.java
+++ src/sample/Controller_wizyty.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import java.awt.*;
+
+import javafx.stage.Stage;
+
 public class Controller_wizyty {
+    Stage primaryStage;
+    @FXML
+    Button buttonLogin, exit_button, minimalize_button;
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        primaryStage.close();
+    }
+
+    public void minimize(ActionEvent actionEvent) {
+        primaryStage.setIconified(true);
+    }
 }
