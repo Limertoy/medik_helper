5fa23d231f9df2a6a09df1099133c895d4a5e726
Limertoy <limertoy@ukr.net>
2021-03-25 20:53:47 +0100

    MH-2 poprawa contrllerów, wszystko działa

--- src/sample/ControllerWizyty.java
+++ src/sample/ControllerWizyty.java
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import java.awt.*;
 import java.io.IOException;
 
 import javafx.scene.control.TableColumn;
