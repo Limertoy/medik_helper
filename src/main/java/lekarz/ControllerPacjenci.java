1a1bcd4eb549396643cb9b7cc42a3fee744cb267
pkulpinski <malpaonev@gmail.com>
2021-04-29 22:47:05 +0200

    MH-43 Dodanie połączenie z bazą danych dla pacjentów

--- src/main/java/lekarz/ControllerPacjenci.java
+++ src/main/java/lekarz/ControllerPacjenci.java
@@ -1,8 +1,12 @@
 package lekarz;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,22 +15,30 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.ResourceBundle;
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import obiekty.Pacjent;
 import org.hibernate.Session;
 import sample.HibernateUtil;
 
-public class ControllerPacjenci {
+import org.hibernate.query.Query;
+
+public class ControllerPacjenci implements Initializable {
     Stage primaryStage;
     @FXML
     Button buttonLogin, exit_button, minimalize_button, zobaczKarte;
     @FXML
-    private TableView<?> table;
+    private TableView<ListaPacjentow> table;
     @FXML
-    private TableColumn<?, ?> nazwisko_table, imie_table, pesel_table, ulica_table, miejscowosc_table;
+    private TableColumn nazwisko_table, imie_table, pesel_table, ulica_table, miejscowosc_table;
+
+    Session session = HibernateUtil.getSessionFactory().openSession();
 
     public void wybierz(MouseEvent mouseEvent) { zobaczKarte.setVisible(true); }
 
@@ -95,4 +107,108 @@ public class ControllerPacjenci {
         window.setScene(kartaPacjentaScene);
         window.show();
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Query<Pacjent> q = session.createQuery("from Pacjent");
+        List<Pacjent> list = q.list();
+
+        ObservableList<ListaPacjentow> data1 = FXCollections.observableArrayList();
+
+        int i = 0;
+        for(Pacjent s : list){
+            data1.add(i, new ListaPacjentow(s.getId_pacjenta(), s.getNazwisko_pacjenta(), s.getImie_pacjenta(), s.getPesel(), s.getAdres(), s.getMiejscowosc()));
+            i++;
+        }
+        nazwisko_table.setCellValueFactory(new PropertyValueFactory("nazwisko_table"));
+        imie_table.setCellValueFactory(new PropertyValueFactory("imie_table"));
+        pesel_table.setCellValueFactory(new PropertyValueFactory("pesel_table"));
+        ulica_table.setCellValueFactory(new PropertyValueFactory("ulica_table"));
+        miejscowosc_table.setCellValueFactory(new PropertyValueFactory("miejscowosc_table"));
+
+        table.getItems().setAll(data1);
+    }
+
+    public class ListaPacjentow{
+        public int id;
+        public SimpleStringProperty nazwisko_table, imie_table, pesel_table, ulica_table, miejscowosc_table;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNazwisko_table() {
+            return nazwisko_table.get();
+        }
+
+        public SimpleStringProperty nazwisko_tableProperty() {
+            return nazwisko_table;
+        }
+
+        public void setNazwisko_table(String nazwisko_table) {
+            this.nazwisko_table.set(nazwisko_table);
+        }
+
+        public String getImie_table() {
+            return imie_table.get();
+        }
+
+        public SimpleStringProperty imie_tableProperty() {
+            return imie_table;
+        }
+
+        public void setImie_table(String imie_table) {
+            this.imie_table.set(imie_table);
+        }
+
+        public String getPesel_table() {
+            return pesel_table.get();
+        }
+
+        public SimpleStringProperty pesel_tableProperty() {
+            return pesel_table;
+        }
+
+        public void setPesel_table(String pesel_table) {
+            this.pesel_table.set(pesel_table);
+        }
+
+        public String getUlica_table() {
+            return ulica_table.get();
+        }
+
+        public SimpleStringProperty ulica_tableProperty() {
+            return ulica_table;
+        }
+
+        public void setUlica_table(String ulica_table) {
+            this.ulica_table.set(ulica_table);
+        }
+
+        public String getMiejscowosc_table() {
+            return miejscowosc_table.get();
+        }
+
+        public SimpleStringProperty miejscowosc_tableProperty() {
+            return miejscowosc_table;
+        }
+
+        public void setMiejscowosc_table(String miejscowosc_table) {
+            this.miejscowosc_table.set(miejscowosc_table);
+        }
+
+        public ListaPacjentow(int id, String nazwisko_table, String imie_table, String pesel_table, String ulica_table, String miejscowosc_table) {
+            this.id = id;
+            this.nazwisko_table = new SimpleStringProperty(nazwisko_table);
+            this.imie_table = new SimpleStringProperty(imie_table);
+            this.pesel_table = new SimpleStringProperty(pesel_table);
+            this.ulica_table = new SimpleStringProperty(ulica_table);
+            this.miejscowosc_table = new SimpleStringProperty(miejscowosc_table);
+        }
+    }
+
 }
