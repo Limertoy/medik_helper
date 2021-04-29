580b577440c24ce41c4696301ab50da6c8d84241
filu4 <filu4@DESKTOP-M7LAPAH>
2021-04-29 18:53:18 +0200

    MH-45 wyświetlenie listy lekrzy i pacjentów

--- src/main/java/obiekty/Pacjent.java
+++ src/main/java/obiekty/Pacjent.java
@@ -20,12 +20,163 @@ public class Pacjent {
     private String drugie_imie;
     private String pesel;
     private String plec;
-    private Date data_urodzeniaa;
+    private Date data_urodzenia;
     private String wojewodztwo;
-    private String mejscowosc;
+    private String miejscowosc;
     private String adres;
     private String kod_pocztowy;
     private String telefon;
     private String email;
 
+    public int getId_pacjenta() {
+        return id_pacjenta;
+    }
+
+    public void setId_pacjenta(int id_pacjenta) {
+        this.id_pacjenta = id_pacjenta;
+    }
+
+    public String getNazwisko_pacjenta() {
+        return nazwisko_pacjenta;
+    }
+
+    public void setNazwisko_pacjenta(String nazwisko_pacjenta) {
+        this.nazwisko_pacjenta = nazwisko_pacjenta;
+    }
+
+    public String getImie_pacjenta() {
+        return imie_pacjenta;
+    }
+
+    public void setImie_pacjenta(String imie_pacjenta) {
+        this.imie_pacjenta = imie_pacjenta;
+    }
+
+    public String getDrugie_imie() {
+        return drugie_imie;
+    }
+
+    public void setDrugie_imie(String drugie_imie) {
+        this.drugie_imie = drugie_imie;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getPlec() {
+        return plec;
+    }
+
+    public void setPlec(String plec) {
+        this.plec = plec;
+    }
+
+    public Date getData_urodzenia() {
+        return data_urodzenia;
+    }
+
+    public void setData_urodzenia(Date data_urodzenia) {
+        this.data_urodzenia = data_urodzenia;
+    }
+
+    public String getWojewodztwo() {
+        return wojewodztwo;
+    }
+
+    public void setWojewodztwo(String wojewodztwo) {
+        this.wojewodztwo = wojewodztwo;
+    }
+
+    public String getMiejscowosc() {
+        return miejscowosc;
+    }
+
+    public void setMiejscowosc(String miejscowosc) {
+        this.miejscowosc = miejscowosc;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getKod_pocztowy() {
+        return kod_pocztowy;
+    }
+
+    public void setKod_pocztowy(String kod_pocztowy) {
+        this.kod_pocztowy = kod_pocztowy;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Pacjent(
+                   String nazwisko_pacjenta,
+                   String imie_pacjenta,
+                   String drugie_imie,
+                   String pesel,
+                   String plec,
+                   Date data_urodzenia,
+                   String wojewodztwo,
+                   String miejscowosc,
+                   String adres,
+                   String kod_pocztowy,
+                   String telefon,
+                   String email) {
+        this.nazwisko_pacjenta = nazwisko_pacjenta;
+        this.imie_pacjenta = imie_pacjenta;
+        this.drugie_imie = drugie_imie;
+        this.pesel = pesel;
+        this.plec = plec;
+        this.data_urodzenia = data_urodzenia;
+        this.wojewodztwo = wojewodztwo;
+        this.miejscowosc = miejscowosc;
+        this.adres = adres;
+        this.kod_pocztowy = kod_pocztowy;
+        this.telefon = telefon;
+        this.email = email;
+    }
+
+    public Pacjent() {}
+
+    @Override
+    public String toString() {
+        return "Pacjent{" +
+                "id_pacjenta=" + id_pacjenta +
+                ", nazwisko_pacjenta='" + nazwisko_pacjenta + '\'' +
+                ", imie_pacjenta='" + imie_pacjenta + '\'' +
+                ", drugie_imie='" + drugie_imie + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", plec='" + plec + '\'' +
+                ", data_urodzenia=" + data_urodzenia +
+                ", wojewodztwo='" + wojewodztwo + '\'' +
+                ", miejscowosc='" + miejscowosc + '\'' +
+                ", adres='" + adres + '\'' +
+                ", kod_pocztowy='" + kod_pocztowy + '\'' +
+                ", telefon='" + telefon + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/main/java/rejestracja/ControllerZarzadzaj.java
+++ src/main/java/rejestracja/ControllerZarzadzaj.java
@@ -14,21 +14,162 @@ import javafx.scene.control.Button;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import obiekty.Pacjent;
+import obiekty.Pracownik;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import sample.HibernateUtil;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ControllerZarzadzaj implements Initializable {
     public TableColumn godzinaTable, ponTable, wtoTable, sroTable, czwTable, piaTable, sobTable, niedTable;
     public TableView<ControllerZarzadzaj.Kalen> kalendarz = new TableView<ControllerZarzadzaj.Kalen>();
+    public TableView<ListaPracownikow> pracownicy;
+    public TableView<ListaPacjentow> pacjenci;
+    public TableColumn imie_table, nazwisko_table,imieTable, nazwiskoTable, peselTable;
     @FXML
     Button buttonLogin, exit_button, minimalize_button;
+    Session session = HibernateUtil.getSessionFactory().openSession();
 
+    public class ListaPracownikow{
+        public int id;
+        public SimpleStringProperty imie_table, nazwisko_table;
+
+        public int getId() {
+            return id;
+        }
+        public int id_rola;
+
+        public ListaPracownikow(int id_rola) {
+            this.id_rola = id_rola;
+        }
+
+        public int getId_rola() {
+            return id_rola;
+        }
+
+        public void setId_rola(int id_rola) {
+            this.id_rola = id_rola;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getImie_table() {
+            return imie_table.get();
+        }
+
+        public SimpleStringProperty imie_tableProperty() {
+            return imie_table;
+        }
+
+        public void setImie_table(String imie_table) {
+            this.imie_table.set(imie_table);
+        }
+
+        public String getNazwisko_table() {
+            return nazwisko_table.get();
+        }
+
+        public SimpleStringProperty nazwisko_tableProperty() {
+            return nazwisko_table;
+        }
+
+        public void setNazwisko_table(String nazwisko_table) {
+            this.nazwisko_table.set(nazwisko_table);
+        }
+
+        public ListaPracownikow(int id, String imie_table, String nazwisko_table, int id_rola) {
+            this.id = id;
+            this.imie_table = new SimpleStringProperty(imie_table);
+            this.nazwisko_table = new SimpleStringProperty(nazwisko_table);
+            this.id_rola = id_rola;
+        }
+    }
+
+    public class ListaPacjentow{
+        public int id;
+        public SimpleStringProperty imieTable;
+        public SimpleStringProperty nazwiskoTable;
+        public SimpleStringProperty peselTable;
+
+        public int getId() { return id; }
+
+        public void setId(int id) { this.id = id; }
+
+        public String getImieTable() { return imieTable.get(); }
+
+        public SimpleStringProperty imieTableProperty() { return imieTable; }
+
+        public void setImieTable(String imieTable) { this.imieTable.set(imieTable); }
+
+        public String getNazwiskoTable() { return nazwiskoTable.get(); }
+
+        public SimpleStringProperty nazwiskoTableProperty() { return nazwiskoTable; }
+
+        public void setNazwiskoTable(String nazwiskoTable) { this.nazwiskoTable.set(nazwiskoTable); }
+
+        public String getPeselTable() { return peselTable.get(); }
+
+        public SimpleStringProperty peselTableProperty() { return peselTable; }
+
+        public void setPeselTable(String peselTable) { this.peselTable.set(peselTable); }
+
+
+
+        public ListaPacjentow(int id, String imieTable, String nazwiskoTable, String peselTable) {
+            this.id = id;
+            this.imieTable = new SimpleStringProperty(imieTable);
+            this.nazwiskoTable =new SimpleStringProperty(nazwiskoTable);
+            this.peselTable =new SimpleStringProperty(peselTable);
+        }
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Query<Pracownik> q = session.createQuery("from Pracownik");
+        List<Pracownik> list = q.list();
+
+        ObservableList<ControllerZarzadzaj.ListaPracownikow> data1 = FXCollections.observableArrayList();
+
+        int i = 0;
+        for(Pracownik s : list){
+            if(s.getRola().getId_rola()== 1){
+            data1.add(i, new ControllerZarzadzaj.ListaPracownikow(s.getId_pracownika(), s.getImie_pracownika(), s.getNazwisko_pracownika(),s.getRola().getId_rola()));
+            i++;
+            }
+        }
+
+        imie_table.setCellValueFactory(new PropertyValueFactory("imie_table"));
+        nazwisko_table.setCellValueFactory(new PropertyValueFactory("nazwisko_table"));
+        pracownicy.getItems().setAll(data1);
+
+        Query<Pacjent> q1 = session.createQuery("from Pacjent");
+        List<Pacjent> list1 = q1.list();
+        ObservableList<ControllerZarzadzaj.ListaPacjentow> data2 = FXCollections.observableArrayList();
+        int j = 0;
+        for(Pacjent s : list1){
+                data2.add(j, new ControllerZarzadzaj.ListaPacjentow(s.getId_pacjenta(), s.getImie_pacjenta(), s.getNazwisko_pacjenta(), s.getPesel()));
+                j++;
+            }
+        imieTable.setCellValueFactory(new PropertyValueFactory("imieTable"));
+        nazwiskoTable.setCellValueFactory(new PropertyValueFactory("nazwiskoTable"));
+        peselTable.setCellValueFactory(new PropertyValueFactory("peselTable"));
+        pacjenci.getItems().setAll(data2);
+
+
+
+        kalendarz.getItems().setAll(this.data);
+        kalendarz.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        kalendarz.getSelectionModel().setCellSelectionEnabled(true);
+    }
 
     public class Kalen {
         public String getGodzina() {
@@ -166,14 +307,14 @@ public class ControllerZarzadzaj implements Initializable {
             new ControllerZarzadzaj.Kalen(3,"9:00", "Maciej Dukacz", "", "", "", " ", " ", "Agata Szkup")
     );
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        kalendarz.getItems().setAll(this.data);
-
-        kalendarz.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        kalendarz.getSelectionModel().setCellSelectionEnabled(true);
-    }
+//    @Override
+//    public void initialize(URL url, ResourceBundle rb) {
+//        // TODO
+//        kalendarz.getItems().setAll(this.data);
+//
+//        kalendarz.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+//        kalendarz.getSelectionModel().setCellSelectionEnabled(true);
+//    }
 
     public void exit(ActionEvent actionEvent) {
         Stage stage = (Stage) exit_button.getScene().getWindow();
