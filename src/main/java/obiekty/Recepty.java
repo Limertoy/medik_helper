0c3dcb5299edbe6b25102b7145d59c5022de468e
Akaroso <glawet23@gmail.com>
2021-06-01 15:05:03 +0200

    Nowa recepta

--- src/main/java/obiekty/Recepty.java
+++ src/main/java/obiekty/Recepty.java
@@ -11,7 +11,24 @@ public class Recepty {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private int id_recepty;
-    private String nazwa;
+    private String opis;
+
+    @OneToOne
+    @JoinColumn(name="id_wyp1")
+    private Wyposazenie wyp1;
+    @OneToOne
+    @JoinColumn(name="id_wyp2")
+    private Wyposazenie wyp2;
+    @OneToOne
+    @JoinColumn(name="id_wyp3")
+    private Wyposazenie wyp3;
+    @OneToOne
+    @JoinColumn(name="id_wyp4")
+    private Wyposazenie wyp4;
+    @OneToOne
+    @JoinColumn(name="id_wyp5")
+    private Wyposazenie wyp5;
+
 
     @OneToOne
     @JoinColumn(name="id_pracownika")
@@ -29,12 +46,12 @@ public class Recepty {
         this.id_recepty = id_recepty;
     }
 
-    public String getNazwa() {
-        return nazwa;
+    public String getOpis() {
+        return opis;
     }
 
-    public void setNazwa(String nazwa) {
-        this.nazwa = nazwa;
+    public void setOpis(String nazwa) {
+        this.opis = nazwa;
     }
 
     public Pracownik getPracownik() {
@@ -51,5 +68,60 @@ public class Recepty {
 
     public void setPacjent(Pacjent pacjent) {
         this.pacjent = pacjent;
+    }
+
+    public Wyposazenie getWyp1() {
+        return wyp1;
+    }
+
+    public void setWyp1(Wyposazenie wyp1) {
+        this.wyp1 = wyp1;
+    }
+
+    public Wyposazenie getWyp2() {
+        return wyp2;
+    }
+
+    public void setWyp2(Wyposazenie wyp2) {
+        this.wyp2 = wyp2;
+    }
+
+    public Wyposazenie getWyp3() {
+        return wyp3;
+    }
+
+    public void setWyp3(Wyposazenie wyp3) {
+        this.wyp3 = wyp3;
+    }
+
+    public Wyposazenie getWyp4() {
+        return wyp4;
+    }
+
+    public void setWyp4(Wyposazenie wyp4) {
+        this.wyp4 = wyp4;
+    }
+
+    public Wyposazenie getWyp5() {
+        return wyp5;
+    }
+
+    public void setWyp5(Wyposazenie wyp5) {
+        this.wyp5 = wyp5;
+    }
+
+    public Recepty(int id_recepty, String opis, Wyposazenie wyp1, Wyposazenie wyp2, Wyposazenie wyp3, Wyposazenie wyp4, Wyposazenie wyp5, Pracownik pracownik, Pacjent pacjent) {
+        this.id_recepty = id_recepty;
+        this.opis = opis;
+        this.wyp1 = wyp1;
+        this.wyp2 = wyp2;
+        this.wyp3 = wyp3;
+        this.wyp4 = wyp4;
+        this.wyp5 = wyp5;
+        this.pracownik = pracownik;
+        this.pacjent = pacjent;
+    }
+
+    public Recepty() {
     }
 }
